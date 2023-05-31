8c7f8f8555b57db7f2a76090afb170dad6afe5bb
Odongo006 <www.henryodongo006@gmail.com>
2023-05-31 10:04:04 +0300

    first commit

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,16 +3,23 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
-import { apiKey } from "../../keys";
 // Your web app's Firebase configuration
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: apiKey,
-    authDomain: "react-blog-b4ada.firebaseapp.com",
-    projectId: "react-blog-b4ada",
-    storageBucket: "react-blog-b4ada.appspot.com",
-    messagingSenderId: "570365255686",
-    appId: "1:570365255686:web:f32cfca9d6dd120dfba387"
+  apiKey: "AIzaSyAe2OizK9jDCvdC2QgKWLv0KlRBINzKIwk",
+  authDomain: "blog-app-fc049.firebaseapp.com",
+  projectId: "blog-app-fc049",
+  storageBucket: "blog-app-fc049.appspot.com",
+  messagingSenderId: "258068525890",
+  appId: "1:258068525890:web:e82ed2a2be34b25607cedb",
+  measurementId: "G-92WGWVEMVX"
 };
+
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
